90fc151b1ae429de726d42342163e4bc8009f4a1
Daniel Solawa <danielsolawa@gmail.com>
2016-09-18 11:01:51 +0200

    test 2

--- src/test/java/org/soloxis/controllers/HomeControllerTest.java
+++ src/test/java/org/soloxis/controllers/HomeControllerTest.java
@@ -1,5 +1,8 @@
 package org.soloxis.controllers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,9 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,6 +19,7 @@ public class HomeControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
+	@Test
 	public void getHello() throws Exception{
 		mvc.perform(MockMvcRequestBuilders.get("/"))
 		.andExpect(status().isOk());
